8c0c75bfdf6119e464e2f81380537cb353af6752
Matthias Patscheider <Weisl@users.noreply.github.com>
2021-09-12 14:25:58 +0300

    Update Version

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ bl_info = {
     "name": "Simple Renaming Panel",
     "description": "This Addon offers the basic functionality of renaming a set of objects",
     "author": "Matthias Patscheider",
-    "version": (1, 5, 0),
+    "version": (1, 5, 1),
     "blender": (2, 83, 0),
     "location": "View3D > Tools ",
     "warning": "",
