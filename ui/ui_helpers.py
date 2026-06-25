e664970f22a230d6bd4bbc9c7709b508e8287e02
Matthias <weisl12@hotmail.de>
2026-06-25 16:59:53 +0300

    Remove unused code

--- ui/ui_helpers.py
+++ ui/ui_helpers.py
@@ -21,18 +21,4 @@ class PREFERENCES_OT_open_addon(bpy.types.Operator):
         prefs = context.preferences.addons[base_package].preferences
         prefs.prefs_tabs = self.prefs_tabs
 
-        import addon_utils
-        mod = addon_utils.addons_fake_modules.get('simple_renaming')
-
-        # mod is None the first time the operation is called :/
-        if mod:
-            mod.bl_info['show_expanded'] = True
-
-            # Find User Preferences area and redraw it
-            for window in bpy.context.window_manager.windows:
-                for area in window.screen.areas:
-                    if area.type == 'USER_PREFERENCES':
-                        area.tag_redraw()
-
-        bpy.ops.preferences.addon_expand(module=self.addon_name)
         return {'FINISHED'}
